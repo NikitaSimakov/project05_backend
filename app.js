87aba9053a77523068b21cc2b3c6b7a99d5ab4d4
NikitaSimakov <mishaaniskin@gmail.com>
2023-08-24 14:51:34 +0300

    Remove routers from "app"

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
-import { categoryRouter } from "./routes/api/categories-router.js";
 
 export const app = express();
 dotenv.config();
@@ -12,7 +11,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-app.use("/api/recipes/category-list", categoryRouter);
+
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
