ded1aa9e6685813e2e66934d034e18c240574f48
ArchGRL <vinokur.olga14@gmail.com>
2023-08-26 14:45:49 +0300

    endpoint /auth

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import logger from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import authRouter from "./routes/api/authRouter.js";
+
 export const app = express();
 dotenv.config();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -11,6 +13,8 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use("/api/auth", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
---
+++ routes/api/authRouter.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+import authController from"../../controllers/auth-controller.js";
+
+import userSchemas from "../../schemas/user-schemas.js";
+
+import {validateBody, authenticate, upload} from "../../middlewares/index.js";
+
+const authRouter = express.Router();
+
+authRouter.post("/register", validateBody(userSchemas.registerSchema, "Incorrectly filled fields"), authController.register);
+
+authRouter.post("/login", validateBody(userSchemas.loginSchema, "Incorrectly filled fields"), authController.logIn);
+
+authRouter.get("/:id", authenticate, authController.getUser);
+
+authRouter.patch("/update", authenticate, upload.single("avatar"), validateBody(userSchemas.updateSchema, "Sorry, update failed your profile."),  authController.updateUser);
+
+authRouter.post("/logout", authenticate, authController.logout);
+
+authRouter.patch("/subscribe", validateBody(userSchemas.userEmailSchema, "Invalid email address"), authController.subscribe);
+
+export default authRouter;
