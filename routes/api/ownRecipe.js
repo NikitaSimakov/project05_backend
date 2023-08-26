95fe528f863cf16ce386d9c63a6d976d6f9703b1
Phokoos <hulkovskij@gmail.com>
2023-08-26 20:30:52 +0300

    test

---
+++ routes/api/ownRecipe.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { authenticate } from "../../middlewares"
+
+const router = express.Router()
+
+// router.use(authenticate)
+
+router.get("/",) // get all recipe
+
+router.post("/") // add new recipe
+
+router.delete("/",
+)
