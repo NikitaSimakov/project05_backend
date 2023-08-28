d069ab5919fbb7dfd8c7c824a5137c455dc8e023
Phokoos <hulkovskij@gmail.com>
2023-08-28 07:26:34 +0300

    Finished work with api for add new cocktails to main base

--- schemas/recipe-schemas.js
+++ schemas/recipe-schemas.js
@@ -2,17 +2,24 @@ import Joi from "joi";
 
 
 const addRecipeSchema = Joi.object({
-	name: Joi.string().required(),
-	category: Joi.string().valid("Homemade Liqueur", "Beer", "Shake", "Punch/Party Drink", "Soft Drink", "Cocoa", "Ordinary Drink", "Shot", "Cocktail", "Other/Unknown", "Coffee/Tea"),
+	drink: Joi.string().required(),
+	drinkAlternate: Joi.string(),
+	tags: Joi.string(),
+	video: Joi.string(),
+	category: Joi.string().valid("Homemade Liqueur", "Beer", "Punch / Party Drink", "Soft Drink", "Shake", "Ordinary Drink", "Shot", "Cocktail", "Other/Unknown", "Coffee / Tea", "Cocoa"),
+	IBA: Joi.string(),
+	alcoholic: Joi.string(),
 	glass: Joi.string().valid("Highball glass", "Cocktail glass", "Old-fashioned glass", "Whiskey Glass", "Collins glass", "Pousse cafe glass", "Champagne flute", "Whiskey sour glass", "Cordial glass", "Brandy snifter", "White wine glass", "Nick and Nora Glass", "Hurricane glass", "Coffee mug", "Shot glass", "Jar", "Irish coffee cup", "Punch bowl", "Pitcher", "Pint glass", "Copper Mug", "Wine Glass", "Beer mug", "Margarita/Coupette glass", "Beer pilsner", "Beer Glass", "Parfait glass", "Mason jar", "Margarita glass", "Martini Glass", "Balloon Glass", "Coupe Glass"),
-	recipePreparation: Joi.string().required(),
-	photoUrl: Joi.string(),
+	instructions: Joi.string().required(),
+	drinkThumb: Joi.string(),
+	describe: Joi.string(),
+	creatorId: Joi.string(),
 	ingredients: Joi.array().min(1).items({
 		title: Joi.string().required(),
 		measure: Joi.string().required(),
 		ingredientThumb: Joi.string().required(),
-		thumbMedium: Joi.string().required(),
-		thumbSmall: Joi.string().required()
+		"thumb-medium": Joi.string().required(),
+		"thumb-small": Joi.string().required()
 	}).required()
 })
 
