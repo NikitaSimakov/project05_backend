36f3af51d47d595459f60ebd7489575d69697c1a
Phokoos <hulkovskij@gmail.com>
2023-08-30 22:32:52 +0300

    Updated add to favorite
    1. Added more tests for req
    2. Added id validation

--- routes/api/favoriteRouter.js
+++ routes/api/favoriteRouter.js
@@ -1,16 +1,16 @@
 import express from "express";
 import favoriteControllers from "../../controllers/favorites-controllers.js";
-import { authenticate } from "../../middlewares/index.js";
+import { authenticate, isValidOwnRecipeId } from "../../middlewares/index.js";
 
 const favoriteRouter = express.Router();
 
 favoriteRouter.use(authenticate)
 
-favoriteRouter.patch("/:recipeId", favoriteControllers.addFavoriteController);
+favoriteRouter.patch("/:recipeId", isValidOwnRecipeId, favoriteControllers.addFavoriteController);
 
 favoriteRouter.get("/", favoriteControllers.getFavoritesController);
 
-favoriteRouter.delete("/:recipeId", favoriteControllers.deleteFromFavorites);
+favoriteRouter.delete("/:recipeId", isValidOwnRecipeId, favoriteControllers.deleteFromFavorites);
 
 
 
